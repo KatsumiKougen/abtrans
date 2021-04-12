c227fa67624fcfe019c06ccf772ef3f7d0b61377
Katsumi Kougen <lesanonyme1001@gmail.com>
2021-04-12 19:31:27 +0700

    Changed the code a bit, now the special vocabulary list is a local variable

--- abtrans.py
+++ abtrans.py
@@ -4,23 +4,22 @@
 # Written by Katsumi Kougen - All rights reserved.
 # Final Fantasy X - (C) 2001-2021 Square Enix.
 
-alb="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-eng="epstiwknuvgclrybxhmdofzqajEPSTIWKNUVGCLRYBXHMDOFZQAJ"
-specialvocab=(
-    "fiend","magic","aeon","airship","yevon","sin","summoner","machina","fayth","blitz","blitzball", # In-game terminology
-    "maester","guardian","spiran","summoners","maesters","fiends","airships","machinas","fayths",
-    "guardians",
-    "spira","besaid","kilika","luca","gagazet","bikanel","zanarkand",                                # Location names
-    "al-bhed","hypello","cactuar","ronso","guado","moogle","chocobo","chocobos",                     # Race names
-    "tidus","yuna","auron","kimahri","wakka","lulu","rikku","paine",                                 # Player characters
-    "seymour","braska","dona","belgemine","gandof","ginnem","isaaru","ohalland","yocun","zuke",      # Non-player characters
-    "yunalesca","barthello","jecht","maroda","pacce","zaon","chappu","clasko","elma","lucil",
-    "luzzu","gatta","mi'ihen","jyscal","kelk","mika","wen-kinoc","cid","rin","keyakku","biran",
-    "yenke","calli","maechen","omega","shelinda",
-    "python","bisqwit","katsumi","shulk","reyn","fiora","dunban","melia","sharla","riki","github",   # Miscellaneous
-)
-
 def trans(text,toAlBhed=True):
+    alb="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    eng="epstiwknuvgclrybxhmdofzqajEPSTIWKNUVGCLRYBXHMDOFZQAJ"
+    specialvocab=(
+        "fiend","magic","aeon","airship","yevon","sin","summoner","machina","fayth","blitz","blitzball", # In-game terminology
+        "maester","guardian","spiran","summoners","maesters","fiends","airships","machinas","fayths",
+        "guardians",
+        "spira","besaid","kilika","luca","gagazet","bikanel","zanarkand",                                # Location names
+        "al-bhed","hypello","cactuar","ronso","guado","moogle","chocobo","chocobos",                     # Race names
+        "tidus","yuna","auron","kimahri","wakka","lulu","rikku","paine",                                 # Player characters
+        "seymour","braska","dona","belgemine","gandof","ginnem","isaaru","ohalland","yocun","zuke",      # Non-player characters
+        "yunalesca","barthello","jecht","maroda","pacce","zaon","chappu","clasko","elma","lucil",
+        "luzzu","gatta","mi'ihen","jyscal","kelk","mika","wen-kinoc","cid","rin","keyakku","biran",
+        "yenke","calli","maechen","omega","shelinda",
+        "python","bisqwit","katsumi","shulk","reyn","fiora","dunban","melia","sharla","riki","github",   # Miscellaneous
+    )
     out=[]
     if toAlBhed:
         wordlist=text.split(" ")
